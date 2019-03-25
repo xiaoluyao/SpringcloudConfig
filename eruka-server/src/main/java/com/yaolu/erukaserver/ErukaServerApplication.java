81a7012f7e9c49b14626b56b397e33ad770bf3fa
yaolu <yl19931017>
2019-03-25 15:59:08 +0800

    config

---
+++ eruka-server/src/main/java/com/yaolu/erukaserver/ErukaServerApplication.java
@@ -0,0 +1,15 @@
+package com.yaolu.erukaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ErukaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErukaServerApplication.class, args);
+	}
+
+}
